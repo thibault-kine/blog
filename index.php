7b6072a891af7882cdf8fabacfb92c082ff667c7
[fredok]gitconfig --global user.email [fredericksonde@gmail.com] <frederick.sonder@laplateforme.io>
2022-01-12 15:53:19 +0100

    debut inscription

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ public function article()
 
 
 
+
 <a href="articles.php">Articles</a>
 <?php
     include 'footer.php';
---
+++ inscription.php
@@ -0,0 +1,24 @@
+<?php
+    include 'header.php';
+?>
+
+
+
+<h1>Inscrivez-vous</h1>
+        <form action="inscription.php" method="POST">
+            <label for="login">Login:</label>
+            <input type="text" name="login" id="login">
+            <label for="email">Email:</label>
+            <input type="text" name="email" id="email">
+            <label for="password">Mot de passe:</label>
+            <input type="password" name="password" id="password">
+            <label for="password2">Confirmez votre mot de passe:</label>
+            <input type="password" name="password2" id="password2">
+            <input type="submit" value="submit" name="submit">
+        </form>
+        <a href="connexion.php">Déjà inscrit?</a>
+
+
+<?php
+    include 'footer.php';
+?>
