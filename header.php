08437527879ee87c59062efc912c25296f044cdd
[fredok]gitconfig --global user.email [fredericksonde@gmail.com] <frederick.sonder@laplateforme.io>
2022-01-12 14:21:45 +0100

    modif fin header

--- header.php
+++ header.php
@@ -15,17 +15,32 @@
 </head>
 <body>
     <header>
-        <a href="livre-or.php">BLOG</a>
+        <h1>BLOG</h1>
         <?php
             if(empty($_SESSION))
             {
                 echo '<a href="connexion.php">Connexion</a>';
                 echo '<a href="inscription.php">Inscription</a>';
             }
-            elsif(!empty($_SESSION) && )
+            // acces admin
+            elseif(!empty($_SESSION) && $_SESSION["droit"]["id"]!="1337")
             {
-                echo '<a href="commentaire.php">Commentaire</a>';
+                echo '<a href="index.php">Acceuil</a>';
+                echo '<a href="profil.php">Profil</a>';
+                echo '<a href="creer-article.php">Creer un article</a>';
+                echo '<a href="admin.php">Administration</a>';
+                echo '<a href="deconnexion.php">Déconnexion</a>';
+
             }
+            //acces modo
+            elseif(!empty($_SESSION) && $_SESSION["droit"]["id"]!="42")
+            {
+                echo '<a href="index.php">Acceuil</a>'; 
+                echo '<a href="profil.php">Profil</a>';
+                echo '<a href="creer-article.php">Creer un article</a>';
+                echo '<a href="deconnexion.php">Déconnexion</a>';
+            }
+            // acces user
             else
             {
                 echo '<a href="index.php">Acceuil</a>';
