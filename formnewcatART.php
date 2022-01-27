245a54818a138896a9cadf6c6a67ca3744421419
[fredok]gitconfig --global user.email [fredericksonde@gmail.com] <frederick.sonder@laplateforme.io>
2022-01-27 12:50:10 +0100

    formulaire newcat

--- formnewcatART.php
+++ formnewcatART.php
@@ -1,10 +1,22 @@
 <?php
     include 'header.php';
     require 'classes/categorie.php';
-    $categorie = new Categorie();
+    $catego = new Categorie();
 ?>
 <form action="" method="post" name="categorie">
     <label for="categorie">Nouvelle categorie</label>
     <input type="text" name="categorie" id="">
-    <input type="submit" value="submit">
+    <input type="submit" name="submit" value="submit">
 </form>
+
+<?php
+    if(isset($_POST['submit']))
+    {
+        $nom = $_POST['categorie'];
+        $catego->register($nom);
+        echo "Nouvelle catégorie validée.";
+        echo "<br><br>";
+        echo "<a href='admin.php'><=RETOUR<=</a>";
+    }
+    include 'footer.php';
+?>
